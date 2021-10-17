880e08b46e00a3690d5f3cbd5e4717bbccc859e9
Matt Toensing <matthew.toensing@gmail.com>
2021-10-17 16:00:48 -0700

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :image_url, presence: true
+  validates :token, presence: true
 end
