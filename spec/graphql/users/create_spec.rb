b794d6100e14fd70994ba84e7c02177e12cac909
Matt Toensing <matthew.toensing@gmail.com>
2021-10-16 17:25:53 -0700

    Create test for user show response, and fix errors in create user test

--- spec/graphql/users/create_spec.rb
+++ spec/graphql/users/create_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe 'User create', type: :request do
     it 'creates new user' do
       post '/graphql', params:{ query: mutation }
       user = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
-      expect(user[:data][:user]).to have_key(:nickname)
-      expect(user[:data][:user]).to have_key(:email)
-      expect(user[:data][:user]).to have_key(:image_url)
-      expect(user[:data][:user]).to have_key(:token)
+
+      expect(user[:data][:createUser][:user]).to have_key(:nickname)
+      expect(user[:data][:createUser][:user]).to have_key(:email)
+      expect(user[:data][:createUser][:user]).to have_key(:imageUrl)
+      expect(user[:data][:createUser][:user]).to have_key(:token)
     end
+
     def mutation
       <<~GQL
       mutation {
