88f5aa94216f8e099a08a3027c59e0340564dd95
Matt Toensing <matthew.toensing@gmail.com>
2021-10-18 21:12:16 -0700

    Create test for logging in with github oauth

---
+++ spec/graphql/login/login_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+ RSpec.describe 'login api', type: :request do
+   before(:each) do
+     @code = "8ad3d15881c331f37229"
+   end
+
+   it 'can create a new user from a github code', :vcr do
+     post '/graphql', params: { query: query }
+
+     expect(response).to be_successful
+
+     user = JSON.parse(response.body, symbolize_names: true)
+
+     expect(user).to have_key(:data)
+     expect(user[:data][:createUser][:user]).to have_key(:nickname)
+     expect(user[:data][:createUser][:user]).to have_key(:imageUrl)
+     expect(user[:data][:createUser][:user]).to have_key(:token)
+   end
+
+   def query
+     <<~GQL
+     mutation {
+       createUser(input: {
+         code: "#{@code}"
+       }) {
+         user {
+       nickname
+       email
+       imageUrl
+       token
+      }
+     }
+   }
+   GQL
+   end
+ end
