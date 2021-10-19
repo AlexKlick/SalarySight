2af40538266b3e0b395fd8e734973daacb1b3f44
Matt Toensing <matthew.toensing@gmail.com>
2021-10-18 20:20:49 -0700

    Make changes to test to reflect new changes to columns on table

--- spec/graphql/posts/index_spec.rb
+++ spec/graphql/posts/index_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'posts request API' do
   it 'can send a list of post data' do
-    post1 = create(:post)
-    post2 = create(:post)
+    user = create(:user)
+    post1 = create(:post, user: user)
+    post2 = create(:post, user: user)
 
     response = SalarySightSchema.execute(query).as_json
 
     response['data']['posts'].each do |post|
       expect(post).to have_key('id')
-      expect(post).to have_key('name')
       expect(post).to have_key('company')
       expect(post).to have_key('salary')
       expect(post).to have_key('previousEducation')
-      expect(post).to have_key('bootCampType')
       expect(post).to have_key('devTitle')
-      expect(post).to have_key('comments')
       expect(post).to have_key('state')
       expect(post).to have_key('jobHuntDuration')
       expect(post).to have_key('gender')
@@ -29,13 +27,10 @@ RSpec.describe 'posts request API' do
     query {
       posts {
         id
-        name
         company
         salary
         previousEducation
-        bootCampType
         devTitle
-        comments
         state
         jobHuntDuration
         gender
