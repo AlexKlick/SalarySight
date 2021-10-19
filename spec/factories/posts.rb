8d83f5282fc1a4874796a937f44a4105e8208cc5
Matt Toensing <matthew.toensing@gmail.com>
2021-10-18 20:20:25 -0700

    Correct factory attributes to reflect new columns on table

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,16 +1,17 @@
 FactoryBot.define do
   factory :post do
-    name { Faker::Name.name }
     company { Faker::Company.name }
     salary { Faker::Number.between(from: 60000, to: 100000) }
     previous_education { "Four Year Degree" }
-    boot_camp_type { 1 }
     dev_title { Faker::Job.title  }
-    comments { Faker::ChuckNorris.fact }
     state { Faker::Address.state }
     job_hunt_duration { 1 }
     gender { 1 }
     age { 1 }
     years_of_experience { 1 }
+    year_graduated { "2019" }
+    type_of_employment { "Full Time" }
+    location_of_employment { Faker::Address.city  }
+    negotiation { 1 }
   end
 end
