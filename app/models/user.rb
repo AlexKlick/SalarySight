bfb88c6e7d836cd8caac10aa3224b5d3a295d5d8
Matt Toensing <matthew.toensing@gmail.com>
2021-10-18 21:11:43 -0700

    Comment out email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
-  
+
   validates :nickname, presence: true
-  validates :email, presence: true
+  # validates :email, presence: true
   validates :image_url, presence: true
   validates :token, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:nickname) }
-    it { should validate_presence_of(:email) }
+    # it { should validate_presence_of(:email) }
     it { should validate_presence_of(:token) }
     it { should validate_presence_of(:image_url) }
   end
