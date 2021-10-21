a18cb6415288e7c35207385fe5652b036fb07daf
Matt Toensing <matthew.toensing@gmail.com>
2021-10-21 13:13:04 -0700

    WIP

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,22 +1,22 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :company, String, null: false
-    field :salary, Integer, null: false
-    field :degree, String, null: false
-    field :position_title, String, null: false
-    field :state, String, null: false
-    field :job_hunt_duration, Integer, null: false
-    field :gender, String, null: false
-    field :age, Integer, null: false
-    field :years_of_experience, Integer, null: false
-    field :grad_year, Integer, null: false
-    field :username, String, null: false
-    field :program, String, null: false
-    field :first_position, String, null: false
-    field :type_of_employment, String, null: false
-    field :location_of_employment, String, null: false
-    field :first_position, String, null: false
-    field :negotiation, String, null: false
+    field :company, String, null: true
+    field :salary, Integer, null: true
+    field :degree, String, null: true
+    field :position_title, String, null: true
+    field :state, String, null: true
+    field :job_hunt_duration, Integer, null: true
+    field :gender, String, null: true
+    field :age, Integer, null: true
+    field :years_of_experience, Integer, null: true
+    field :grad_year, Integer, null: true
+    field :username, String, null: true
+    field :program, String, null: true
+    field :first_position, String, null: true
+    field :type_of_employment, String, null: true
+    field :location_of_employment, String, null: true
+    field :first_position, String, null: true
+    field :negotiation, String, null: true
   end
 end
