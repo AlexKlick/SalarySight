a46018aeb0b1c1d7ec4c566162cf981a49794d9a
Matt Toensing <matthew.toensing@gmail.com>
2021-10-21 20:53:29 -0700

    Make changes to enums and posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_005454) do
+ActiveRecord::Schema.define(version: 2021_10_22_035007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2021_10_21_005454) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "grad_year"
-    t.integer "negotiation"
     t.bigint "user_id"
     t.string "username"
     t.integer "program"
@@ -36,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_10_21_005454) do
     t.string "gender"
     t.integer "first_position"
     t.integer "location_of_employment"
+    t.string "negotiation"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
