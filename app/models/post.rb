d2ef43d1cd07f9b05ea41c021aaa0d1246e78055
Matt Toensing <matthew.toensing@gmail.com>
2021-10-21 18:47:51 -0700

    Remove validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
   # belongs_to :user
 
-  validates :gender, presence: true
-  validates :age, presence: true
-  validates :grad_year, presence: true
-  validates :degree, presence: true
-  validates :years_of_experience, presence: true
-  validates :state, presence: true
-  validates :salary, presence: true
-  validates :position_title, presence: true
-  validates :type_of_employment, presence: true
-  validates :location_of_employment, presence: true
-  validates :negotiation, presence: true
+  # validates :gender, presence: true
+  # validates :age, presence: true
+  # validates :grad_year, presence: true
+  # validates :degree, presence: true
+  # validates :years_of_experience, presence: true
+  # validates :state, presence: true
+  # validates :salary, presence: true
+  # validates :position_title, presence: true
+  # validates :type_of_employment, presence: true
+  # validates :location_of_employment, presence: true
+  # validates :negotiation, presence: true
 
   enum program: {backend: 0, frontend: 1}
   enum first_position: {yes: 0, no: 1}
