56eb023e903dc8fe0bd386e1a6ffac31c0995605
Matt Toensing <matthew.toensing@gmail.com>
2021-10-23 08:53:52 -0700

    Change response for post type to be all strings

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,15 +2,15 @@ module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
     field :company, String, null: true
-    field :salary, Integer, null: true
+    field :salary, String, null: true
     field :degree, String, null: true
     field :position_title, String, null: true
     field :state, String, null: true
-    field :job_hunt_duration, Integer, null: true
+    field :job_hunt_duration, String, null: true
     field :gender, String, null: true
-    field :age, Integer, null: true
-    field :years_of_experience, Integer, null: true
-    field :grad_year, Integer, null: true
+    field :age, String, null: true
+    field :years_of_experience, String, null: true
+    field :grad_year, String, null: true
     field :username, String, null: true
     field :program, String, null: true
     field :first_position, String, null: true
