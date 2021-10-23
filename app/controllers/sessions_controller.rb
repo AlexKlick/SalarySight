87de8b6784f59214dff228a82d16c0e9a3607b51
Matt Toensing <matthew.toensing@gmail.com>
2021-10-23 13:31:47 -0700

    Add sessions controller with redirect ready to go when it is ready on fe

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    token = GithubService.get_user_access_token(params[:code])
+    user_info = GithubService.get_user_information(token[:access_token])
+
+    user = User.find_or_create_by(nickname: user_info[:login])
+    user.update(image_url: user_info[:avatar_url], token: token[:access_token])
+
+    redirect_to "https://salary-sight.herokuapp.com/#{user.nickname}"
+  end
+end
